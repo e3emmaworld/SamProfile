483e6acf659274f59ea2aa831c4250d02ef9c8a3
Emmanuel Edet Effiong <168737077+e3emmaworld@users.noreply.github.com>
2025-02-26 14:21:16 +0100

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,42 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been sent.");
+});
+
+const word = "Samuel Effiom";
+let currentText = "";
+let index = 0;
+
+function spellWord() {
+    if (index < word.length) {
+        currentText += word[index];  // Add one letter at a time
+        document.getElementById("output").textContent = currentText;
+        index++;
+        setTimeout(spellWord, 500); // Repeat every 500ms
+    }
+}
+
+spellWord(); 
+
+const box = document.getElementById('rotating-border');
+
+// Array of colors to rotate through
+const colors = ['red','black', 'pink', 'blue', 'green', 'yellow', 'purple', 'orange','white'];
+
+let colorIndex = 0;
+
+// Function to change border color
+function rotateBorderColor() {
+  box.style.borderColor = colors[colorIndex];
+  colorIndex = (colorIndex + 1) % colors.length; // Cycle through colors
+}
+
+// Rotate border color every second
+setInterval(rotateBorderColor, 1000);
+
+function playVideo() {
+    document.getElementById("myVideo").play();
+}
+function pauseVideo() {
+    document.getElementById("myVideo").pause();
+}
